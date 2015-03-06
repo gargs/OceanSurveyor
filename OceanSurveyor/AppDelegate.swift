aa6c15c40e1065d3ea55857bcb534701f9829ad6
Saurabh Garg <gargs@Saurabhs-iMac.local>
2015-03-06 19:03:45 +0100

    Initial Commit

---
+++ OceanSurveyor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OceanSurveyor
+//
+//  Created by Saurabh Garg on 06/03/15.
+//  Copyright (c) 2015 Saurabh Garg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OceanSurveyor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OceanSurveyor
+//
+//  Created by Saurabh Garg on 06/03/15.
+//  Copyright (c) 2015 Saurabh Garg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
